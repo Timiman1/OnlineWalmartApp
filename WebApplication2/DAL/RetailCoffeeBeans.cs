691fb2da156388f656f3a138db98dbafc96ef7ed
Joakim <joakimforslund@hotmail.com>
2022-04-20 15:22:19 +0200

    Added Coffee Products

---
+++ WebApplication2/DAL/RetailCoffeeBeans.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineWalmart.DAL
+{
+    public class RetailCoffeeBeans : Product
+    {
+        public override string Name { get;  }
+
+        public override string Description { get; }
+
+        public override IPrice Price { get; }
+
+        public override string DeliveryInfo { get; }
+    }
+}
