cc47013a351be6a50f6687aa47277b99b6e85969
Tim Tim Toresson <timtoresson@hotmail.com>
2022-04-20 14:28:16 +0200

    Add Price class with options to get and set discount

--- WebApplication2/DAL/IProduct.cs
+++ WebApplication2/DAL/IProduct.cs
@@ -1,10 +1,11 @@
-﻿namespace WebApplication2.DAL
+﻿using OnlineWalmart.DAL;
+
+namespace WebApplication2.DAL
 {
     public interface IProduct
     {
         string Name { get; }
         string Description { get; }
-        double Price { get; }
-
+        Price Price { get; }
     }
 }
---
+++ WebApplication2/DAL/Price.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineWalmart.DAL
+{
+    public class Price
+    {
+        public double Value { get; set; }
+
+        public double GetDiscountedPrice(double percentage)
+        {
+            return Value * percentage / 100.0;
+        }
+        public void SetDiscountedPrice(double percentage)
+        {
+            Value = Value * percentage / 100.0;
+        }
+    }
+}
