218d12c0f92eec2c8aefd075da2a67700279c18e
Joakim <joakimforslund@hotmail.com>
2022-04-20 15:41:31 +0200

    Changed code in Coffee Products

--- WebApplication2/DAL/OnlineCoffeeBeans.cs
+++ WebApplication2/DAL/OnlineCoffeeBeans.cs
@@ -2,6 +2,13 @@
 {
     public class OnlineCoffeeBeans : OnlineProduct
     {
+        public OnlineCoffeeBeans(string name, string description, IPrice price)
+        {
+            Name=name;
+            Description=description;
+            Price=price;
+        }
+
         public override string Name { get; }
 
         public override string Description { get; }
--- WebApplication2/DAL/RetailCoffeeBeans.cs
+++ WebApplication2/DAL/RetailCoffeeBeans.cs
@@ -2,6 +2,13 @@
 {
     public class RetailCoffeeBeans : RetailProduct
     {
+        public RetailCoffeeBeans(string name, string description, IPrice price)
+        {
+            Name=name;
+            Description=description;
+            Price=price;
+        }
+
         public override string Name { get;  }
 
         public override string Description { get; }
