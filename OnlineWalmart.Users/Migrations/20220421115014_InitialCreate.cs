c69de3ed56b563a27410a273cab40a2a578b4638
Kamil Niescieronek <kamil.niescieronek@iths.se>
2022-04-21 14:52:31 +0200

    Branched out. Added User Microservice

---
+++ OnlineWalmart.Users/Migrations/20220421115014_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineWalmart.Users.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "DateOfBirth", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[] { new Guid("2c53aafa-e8e2-4496-89b4-6b9a4d301c03"), new DateTimeOffset(new DateTime(1982, 6, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "john@nomail.com", "John", "Doe", "0765628331" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "DateOfBirth", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[] { new Guid("361efbef-13bd-44fe-929d-917e71d0e5e7"), new DateTimeOffset(new DateTime(1995, 8, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "jane@nomail.com", "Jane", "Doe", "0765628331" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "DateOfBirth", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[] { new Guid("a93b839a-14dc-4d36-8b1e-6119fafe9d5a"), new DateTimeOffset(new DateTime(1985, 4, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "billy@nomail.com", "Billy", "Bob", "0763648234" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
